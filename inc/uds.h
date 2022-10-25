929c8fc51bf00f060ce15604a7880031b521edbd
90509199 <wangzhiwen2@mychery.com>
2022-10-25 09:52:13 +0800

    增加debug和log打印

--- inc/uds.h
+++ inc/uds.h
@@ -1,6 +1,14 @@
 #ifndef _UDS_H_
 #define _UDS_H_
 #include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n",\
+        __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
+        __FILE__, __LINE__, ##__VA_ARGS__)
 
 typedef enum
 {
